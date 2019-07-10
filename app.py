05a4f3fc2143b5eb80dd42e0c055ba582b92d23c
CASDOP <51396975+CASDOP@users.noreply.github.com>
2019-07-10 14:17:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def callback():
     json_line = request.get_json()
     json_line = json.dumps(json_line)
     decoded = json.loads(json_line)
-    userText = decoded['queryresult']['intent']['displayName']
+    user = decoded['originalDeleetIntentResult]['playload']['data']['replyToken']
     userText = decoded['queryresult']['intent']['displayName']
     #sendText(user,userText)
     if (userText == 'สวัสดี') :
